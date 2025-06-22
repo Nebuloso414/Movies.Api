286d398a40e5c760ff8808365137bdbd250f2555
Guillermo Garcia <g2beastie@live.com>
2025-06-22 11:11:54 -0400

    Add project files.

---
+++ Movies.Application/ApplicationServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repository;
+
+namespace Movies.Application
+{
+    public static class ApplicationServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddSingleton<IMovieRepository, MovieRepository>();
+
+            return services;
+        }
+    }
+}
