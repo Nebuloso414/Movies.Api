1fad18dbc3f361440a67664b9a7c18d20a8106ef
Guillermo Garcia <g2beastie@live.com>
2025-06-22 14:12:40 -0400

    Added support for Postgres database running in a Docker container

---
+++ Movies.Application/Database/IDBConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Movies.Application.Database
+{
+    public interface IDBConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+    }
+
+    public class NpgsqlConnectionFactory : IDBConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public NpgsqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
