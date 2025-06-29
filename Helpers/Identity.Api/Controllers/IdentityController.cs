da33225dd9090c1dcc67c2a88941bc64804b5b07
Guillermo Garcia <g2beastie@live.com>
2025-06-29 13:09:45 -0400

    domain update

--- Helpers/Identity.Api/Controllers/IdentityController.cs
+++ Helpers/Identity.Api/Controllers/IdentityController.cs
@@ -47,8 +47,8 @@ public class IdentityController : ControllerBase
         {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.Add(TokenLifetime),
-            Issuer = "https://id.nickchapsas.com",
-            Audience = "https://movies.nickchapsas.com",
+            Issuer = "https://id.nebula.com",
+            Audience = "https://movies.nebula.com",
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
         
